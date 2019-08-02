74e9a5c38f3636604198f44303dd7799fd92fec7
Arvid Norberg <arvid@cs.umu.se>
2019-08-02 08:00:32 -0700

    fix typo in test

--- test/setup_transfer.cpp
+++ test/setup_transfer.cpp
@@ -444,7 +444,7 @@ pid_type async_run(char const* cmdline)
 	startup.cb = sizeof(startup);
 	startup.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
 	startup.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-	startup.hStdError = GetStdHandle(STD_INPUT_HANDLE);
+	startup.hStdError = GetStdHandle(STD_OUTPUT_HANDLE);
 	int ret = CreateProcessA(NULL, buf, NULL, NULL, TRUE
 		, CREATE_NEW_PROCESS_GROUP, NULL, NULL, &startup, &pi);
 
